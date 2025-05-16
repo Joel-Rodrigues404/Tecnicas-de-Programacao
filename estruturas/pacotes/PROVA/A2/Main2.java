6f3035843e1df5a7e81ca27ed07068266c7117cb
Joel-Rodrigues404 <joelsandersonrodrigues@gmail.com>
2025-05-16 20:27:34 -0300

    Entendendo como funcionam os pacotes de java

---
+++ estruturas/pacotes/PROVA/A2/Main2.java
@@ -0,0 +1,7 @@
+package A2;
+
+public class Main2 {
+    public static void main(String[] args) {
+        System.out.println("Main 2");
+    }
+}
---
+++ estruturas/pacotes/PROVA/A3/Main3.java
@@ -0,0 +1,7 @@
+package A3;
+
+public class Main3 {
+    public static void main(String[] args) {
+        System.out.println("Main 3");
+    }
+}
